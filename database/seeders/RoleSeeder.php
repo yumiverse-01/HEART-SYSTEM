fb6d5273e71cfc50175bc26c554f5651bbd48d2c
yumiverse-01 <ayumikarellediza@thelewiscollege.edu.ph>
2026-05-09 13:31:51 +0800

    Removed super admin from role seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,21 +14,6 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::updateOrCreate(
-            ['name' => 'Super Admin'],
-            [
-                'description' => 'Full access to all system features and settings.',
-                'permissions' => [
-                    'view-beneficiaries', 'create-beneficiaries', 'edit-beneficiaries', 'delete-beneficiaries',
-                    'view-events', 'create-events', 'edit-events', 'delete-events',
-                    'view-attendance', 'mark-attendance',
-                    'view-service-records', 'create-service-records', 'edit-service-records', 'delete-service-records',
-                    'view-reports', 'export-reports',
-                    'access-admin', 'manage-users',
-                ],
-            ]
-        );
-
-        Role::updateOrCreate(
             ['name' => 'Admin'],
             [
                 'description' => 'Access to most features with some restrictions on user management.',
